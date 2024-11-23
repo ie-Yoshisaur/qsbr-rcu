abc2cb57dd0b0e8b7264d18151eed456fe1c1b1b
Yoshiaki Sano <yoshiaki.sano@labbase.co.jp>
2024-11-23 17:42:11 +0900

    Add callback test

--- tests/rcu_tests.rs
+++ tests/rcu_tests.rs
@@ -1,46 +1,85 @@
-use read_copy_update::Rcu;
+use read_copy_update::{call_rcu, define_rcu, synchronize_rcu, Rcu};
 use std::thread;
 
-use read_copy_update::define_rcu;
-
 // Define a static RCU instance for i32 with an initial value of 42.
-define_rcu!(RCU_INT, get_rcu_int, i32, 42);
+define_rcu!(RCU_TEST_MULTI, get_rcu_test_multi, i32, 42);
+define_rcu!(RCU_TEST_CALLBACK, get_rcu_test_callback, i32, 42);
 
-/// Increments the value stored in the RCU-protected i32 by 1.
+/// Increment the value stored in the RCU instance.
 /// Uses `try_update` to safely perform the update.
 fn increment_rcu_value(rcu: &Rcu<i32>) {
     rcu.try_update(|current| current + 1).unwrap();
 }
 
+/// Simulate a heavy read workload.
+fn simulate_read(rcu: &Rcu<i32>, iterations: usize) {
+    for _ in 0..iterations {
+        let _ = rcu.read(|val| {
+            assert!(*val >= 42);
+        });
+    }
+}
+
 #[test]
-fn test_rcu_increment() {
-    let rcu = get_rcu_int();
+fn test_rcu_multithreaded_update_and_callback() {
+    let rcu = get_rcu_test_multi();
 
     // Define the number of threads and the number of increments each thread will perform.
     let num_threads = 10;
-    let increments_per_thread = 1000;
+    let increments_per_thread = 100;
+    let read_iterations = 1000;
 
     let mut handles = Vec::new();
 
     // Spawn multiple threads to perform concurrent increments.
     for _ in 0..num_threads {
-        let rcu_ref = rcu;
+        let rcu_clone = rcu;
         let handle = thread::spawn(move || {
             for _ in 0..increments_per_thread {
-                increment_rcu_value(rcu_ref);
+                increment_rcu_value(rcu_clone);
             }
         });
         handles.push(handle);
     }
 
-    // Wait for all threads to finish execution.
+    for _ in 0..num_threads {
+        let rcu_clone = rcu;
+        let handle = thread::spawn(move || {
+            simulate_read(rcu_clone, read_iterations);
+        });
+        handles.push(handle);
+    }
+
     for handle in handles {
         handle.join().unwrap();
     }
 
     // Read the final value from the RCU-protected data.
+    synchronize_rcu();
+    call_rcu(&rcu);
+
     let final_value = rcu.read(|d| *d).unwrap();
     println!("Final value: {}", final_value);
     // Verify that the final value matches the expected result.
     assert_eq!(final_value, 42 + num_threads * increments_per_thread);
+
+    println!("Test passed. Final value: {}", final_value);
+}
+
+#[test]
+fn test_rcu_callback_processing() {
+    let rcu = get_rcu_test_callback();
+
+    for _ in 0..10 {
+        increment_rcu_value(rcu);
+    }
+
+    synchronize_rcu();
+    call_rcu(&rcu);
+
+    rcu.read(|val| {
+        println!("Value after callback processing: {}", val);
+        assert!(*val >= 42);
+    })
+    .unwrap();
 }
